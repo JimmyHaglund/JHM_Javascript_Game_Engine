99818385ce91650780ff3ee77dfeb6fb2079fa9c
JimmyHaglund <jimmyhaglund@gmail.com>
2020-02-12 09:58:05 +0100

    Implement menu - game transition

--- JHM-Framework/javascript/_Happrint/Happrint-MainMenu.js
+++ JHM-Framework/javascript/_Happrint/Happrint-MainMenu.js
@@ -4,9 +4,11 @@ class MainMenu {
         this.onStartGame = new Action();
         this._input = new MouseInput();
         this._loop = new Loop(60);
-        this._renderSpace = new RenderSpace(this._loop, 1200, 1200, 0, 0);
+        let width = screen.width * 0.6;
+        let height = screen.height * 0.6;
+        this._renderSpace = new RenderSpace(this._loop, width, height, 0, 0);
         this._uiSpace = new UiSpace(this._renderSpace, 0, this._input);
-        this.startButton = this._uiSpace.createButton(0, 0, 120, 200, "playButton_normal", "playButton_hover", "playButton_press");
+        this.startButton = this._uiSpace.createButton(width * 0.5 - 100, height * 0.1, 200, 150, "playButton_normal", "playButton_hover", "playButton_press");
         this._loop.update();
         this.startButton.onClick.add(this.startGame, this);
     }
@@ -15,7 +17,7 @@ class MainMenu {
         this.startButton.destroy();
     }
     startGame() {
-        this.destroy();
         this.onStartGame.invoke.call(this.onStartGame);
+        this.destroy();
     }
 }
