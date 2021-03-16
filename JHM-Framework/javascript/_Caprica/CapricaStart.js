2dc40c0e55bb1f36d40ca90997ee71e781941e96
Jimmy <Jimmyhaglund@gmail.com>
2021-03-16 17:58:08 +0100

    Add separate loops for rendering & gameplay

--- JHM-Framework/javascript/_Caprica/CapricaStart.js
+++ JHM-Framework/javascript/_Caprica/CapricaStart.js
@@ -1,41 +1,29 @@
 let movementInput;
 let gameData;
 function capricaStart() {
-    let loop = new Loop(60);
+    let gameLoop = new Loop(60);
+    let renderLoop = new Loop(60);
     let renderLayers = createRenderLayers();
     let cameraTransform = new Transform(0, 0);
-    let camera = createCamera(renderLayers, cameraTransform, loop);
-    // Heres' a volatile situation: the AimConeController will cache the game loop and dynamically add and remove itself
-    // when the user starts respectively stops aiming.
-    // As a result, the aimconecontroller can be assumed to always be last in the update loop.
-    // The aimconecontroller updates its position in its loop, while the character updates its positions as part of the physics loop.
-    // Meanwhile, they both render as part of the camera render update loop.
-    // As a result, if the physics loop is added before the rendering loop the order would be
-    // character move -> render -> aim cone move. The aim cone will seem to lag behind the character.
-    // This is the volatile part: simply changing the order that loops are added will cause a bug.
-    // The solution would be to separate the game/position and the render loops in a better way, either
-    // by explicitly defining two separate loops, or by refactoring/restructuring the aim cone to work better with existing systems.
-    let physicsSpace = new PhysicsSpace(loop);
-    let mainCharacter = new CapricaMainCharacter(0, 0, loop, renderLayers[1], camera, physicsSpace);
+    let camera = createCamera(renderLayers, cameraTransform, renderLoop);
+    let physicsSpace = new PhysicsSpace(gameLoop);
+    let mainCharacter = new CapricaMainCharacter(0, 0, gameLoop, renderLayers[1], camera, physicsSpace);
     cameraTransform.parent = mainCharacter.entity.transform;
     movementInput = mainCharacter.input;
     gameData = {
-        playLoop: loop,
+        playLoop: gameLoop,
         renderLayers: renderLayers,
         physics: physicsSpace,
         mainCharacter: mainCharacter,
         lookController: null
     };
     runDevTests();
-    // let secondCamera = createCamera(renderLayers, new Transform(200, 200), loop, 
-    // 820, 10, 360, 240);
     console.log("Caprica Started");
-    onMouseDown.add(() => generateShaker(cameraTransform), null);
 }
 function generateShaker(transform) {
-    let shakeDisplacement = { min: 5, max: 10 };
+    let shakeDisplacement = { min: 5, max: 20 };
     let loop = gameData.playLoop;
-    let shaker = new Shaker(transform, loop, 1, shakeDisplacement);
+    let shaker = new Shaker(transform, loop, 10, shakeDisplacement);
 }
 function createCamera(renderLayers, transform, loop, left = 10, top = 10, width = 800, height = 600) {
     let camera = new Camera(renderLayers, transform, loop);
